5910e38c0cff4258c47e482e2a66ee530cb50fef
Angelo Putignano <angelo.putignano.16@gmail.com>
2024-12-10 00:21:53 +0100

    Addedd Pyppeteer

--- Src/Utilities/fake_browser.py
+++ Src/Utilities/fake_browser.py
@@ -1,55 +1,73 @@
-import asyncio
+import glob
 import json
+import os
+import zipfile
 from urllib.parse import urlparse
 
-from playwright.async_api import async_playwright
+from pyppeteer import launch
+
+chromium_path = os.path.join(os.getcwd(), "browser", "chrome")
+
+
+def unzip():
+    extract_path = os.path.join(os.getcwd(), "browser")
+    if not os.path.exists(os.path.join(extract_path, "chrome")):
+        print("Unzip browser")
+
+        zip_prefix = os.path.join(extract_path, "chromium.zip.")
+        parts = glob.glob(zip_prefix + '*')
+        n = len(parts)
+
+        temp_zip_name = os.path.join(extract_path, "chromium.zip")
+        with open(temp_zip_name, "wb") as outfile:
+            for i in range(1, n + 1):
+                filename = zip_prefix + str(i).zfill(3)
+                with open(filename, "rb") as infile:
+                    outfile.write(infile.read())
+
+        with zipfile.ZipFile(temp_zip_name, "r") as zip_ref:
+            zip_ref.extractall(extract_path)
+
+        os.remove(temp_zip_name)
+
+    print("Browser ready")
 
 
 async def execute(api: str, more_headers: dict = None, get_json: bool = True):
-    async with async_playwright() as ap:
-        browser = await ap.chromium.launch(headless=True)
-        context = await browser.new_context()
-        await context.clear_cookies()
-        page = await context.new_page()
+    api_response = None
 
-        api_response = None
+    parsed_url = urlparse(api)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
-        async def handle_response(response):
-            nonlocal api_response
-            if response.url == api and response.status == 200:
-                if get_json:
-                    api_response = await response.json()
-                else:
-                    api_response = await response.text()
+    browser = await launch(headless=True, executablePath=chromium_path, args=["--no-sandbox", "--disable-setuid-sandbox"])
+    page = await browser.newPage()
 
-        page.on("response", handle_response)
+    await page.goto(base_url)
 
-        parsed_url = urlparse(api)
-        base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        "Accept": "application/json",
+        "Referer": f"{base_url}",
+        "Origin": f"{base_url}"
+    }
+    if more_headers is not None:
+        headers.update(more_headers)
 
-        await page.goto(base_url)
-
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-            "Accept": "application/json",
-            "Referer": f"{base_url}",
-            "Origin": f"{base_url}"
-        }
-        if more_headers is not None:
-            headers.update(more_headers)
-
-        await page.evaluate(f"""
-            fetch("{api}", {{
+    fetch_script = f"""
+        () => {{
+            return fetch("{api}", {{
                 method: "GET",
                 headers: {json.dumps(headers)}
-            }}).then(response => response).then(console.log);
-        """)
+            }})
+            .then(response => {"response.json()" if get_json else "response.text()"})
+        }}
+    """
 
-        for _ in range(50):
-            if api_response:
-                break
-            await asyncio.sleep(0.1)
-
+    try:
+        api_response = await page.evaluate(fetch_script)
+    except Exception as e:
+        print(f"Fetch Error: {e}")
+    finally:
         await browser.close()
 
     return api_response
--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import  Src.Utilities.config as config
 import logging
 from Src.API.okru import okru_get_url
 from Src.API.animeworld import animeworld
+from Src.Utilities import fake_browser
 from Src.Utilities.dictionaries import okru,STREAM,extra_sources,webru_vary,webru_dlhd,provider_map,skystreaming
 from Src.API.epg import tivu, tivu_get,epg_guide,convert_bho_1,convert_bho_2,convert_bho_3
 from Src.API.webru import webru,get_skystreaming
@@ -298,6 +299,7 @@ async def addon_stream(request: Request,config, type, id,):
 
 
 if __name__ == '__main__':
+    fake_browser.unzip()
     import uvicorn
     uvicorn.run("run:app", host=HOST, port=PORT, log_level="info")
     
