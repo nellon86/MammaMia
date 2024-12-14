244059000a22e8da07941109320361f0d5c5e95d
Angelo Putignano <angelo.putignano.16@gmail.com>
2024-12-14 23:16:59 +0100

    removed pyppeteer

--- Src/Utilities/fake_browser.py
+++ Src/Utilities/fake_browser.py
@@ -27,52 +27,6 @@ async def close():
         await playwright.stop()
         playwright = None
 
-'''
-async def execute_pyppeteer(api: str, more_headers: dict = None, get_json: bool = True):
-    api_response = None
-
-    parsed_url = urlparse(api)
-    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-
-    browser = await launch(headless=True,
-                           executablePath=chromium_path,
-                           args=["--no-sandbox", '--disable-gpu', '--disable-dev-shm-usage'],
-                           dumpio=True)
-    page = await browser.newPage()
-
-    await page.goto(base_url, waitUntil='domcontentloaded', timeout=0)
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-        "Accept": "application/json" if get_json else "text/html",
-        "Referer": f"{base_url}",
-        "Origin": f"{base_url}"
-    }
-    if more_headers is not None:
-        headers.update(more_headers)
-
-    fetch_script = f"""
-        () => {{
-            return fetch("{api}", {{
-                method: "GET",
-                headers: {json.dumps(headers)}
-            }})
-            .then(response => {"response.json()" if get_json else "response.text()"})
-        }}
-    """
-
-    try:
-        api_response = await page.evaluate(fetch_script)
-    except Exception as e:
-        print(f"Fetch Error: {e}")
-    finally:
-        await browser.close()
-
-    return api_response
-
-'''
-
-
 async def execute(api: str, more_headers: dict = None, get_json: bool = True):
     global browser
     await setup_browser()
